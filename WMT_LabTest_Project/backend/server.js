5b3bf38f5751561d794a86e0e2048c4971eb210f
IT24100378 <it24100378@my.sliit.lk>
2026-04-26 01:31:52 +0530

    Remove DNS module and server configuration
    Removed unused DNS module and server configuration.

--- WMT_LabTest_Project/backend/server.js
+++ WMT_LabTest_Project/backend/server.js
@@ -1,8 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
-import dns from "node:dns";
-dns.setServers(["8.8.8.8", "1.1.1.1"]);
 import mongoose from "mongoose";
 import itemRoutes from "./routes/itemRoutes.js";
 
